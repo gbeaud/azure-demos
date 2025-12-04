f67a31c2f564b8050074109d7c28fd1d599ae2e7
Guillaume Beaud <gbeaud@microsoft.com>
2025-12-04 17:17:20 +0100

    Update Run-Script.ps1 to mask subscription ID and add Linux verification command

--- policies-to-deploy-scripts-on-vms/Run-Script.ps1
+++ policies-to-deploy-scripts-on-vms/Run-Script.ps1
@@ -2,10 +2,13 @@
 Connect-AzAccount
 
 # Select subscription (Sandbox - new tenant Sweden Central)
-Set-AzContext -Subscription 3ef3978c-b123-4b9b-92cd-15d940a22dc9
+Set-AzContext -Subscription XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
 
 # Run the verification
 .\Verify-Policy.ps1 -ResourceGroupName "rg-policy-vm-script-dev-swec-01" -VMName "vm-win-demo-01"
 
-# Run the verification manually using Run Command in Azure
+# Run the verification manually using Run Command in Azure for Windows
 Get-ChildItem -Path "C:\" -Force
+
+# Run the verification manually using Run Command in Azure for Linux
+ls -la /
